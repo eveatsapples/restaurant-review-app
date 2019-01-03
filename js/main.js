e36bbbf1cce19f4fd2ca83b387fe41cd052298e6
Everett Penner <everettpenner@codebusiness.ca>
2019-01-03 16:20:38 -0600

    uncommented service worker

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ let restaurants,
 var newMap
 var markers = []
 
-/*
 if(navigator.serviceWorker) {
     navigator.serviceWorker.register('sw.js').then(function() {
         console.log('service worker installed');
@@ -12,7 +11,6 @@ if(navigator.serviceWorker) {
         console.log('service worker installation incomplete');
     });
 };
-*/
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
