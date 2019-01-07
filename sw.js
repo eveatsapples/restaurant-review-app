e943e2243f3830f0d2bae83732e0815961ef5b65
Everett Penner <everettpenner@codebusiness.ca>
2019-01-07 09:44:33 -0600

    added ./ to manifest file paths

--- sw.js
+++ sw.js
@@ -22,25 +22,26 @@ self.addEventListener('install', event => {
         caches.open(staticCache).then(function(cache) {
             return cache.addAll(
                 [
-                    'img/1.jpg',
-		    'img/2.jpg',
-		    'img/3.jpg',
-		    'img/4.jpg',
-		    'img/5.jpg',
-		    'img/6.jpg',
-                    'img/7.jpg',
-                    'img/8.jpg',
-                    'img/9.jpg',
-                    'img/10.jpg',
-                    'img/favicon.png',
+                    './img/1.jpg',
+		    './img/2.jpg',
+		    './img/3.jpg',
+		    './img/4.jpg',
+		    './img/5.jpg',
+		    './img/6.jpg',
+                    './img/7.jpg',
+                    './img/8.jpg',
+                    './img/9.jpg',
+                    './img/10.jpg',
+                    './img/favicon.png',
                     './',
                     './index.html',
-                    'restaurant.html',
-		    'css/styles.css',
-		    'js/main.js',
-                    'js/dbhelper.js',
-                    'js/restaurant_info.js',
-                    'data/restaurants.json'
+                    './restaurant.html',
+		    './css/styles.css',
+		    './js/main.js',
+                    './js/dbhelper.js',
+                    './js/restaurant_info.js',
+                    './data/restaurants.json',
+                    './manifest.json'
                 ]
             );
         })
